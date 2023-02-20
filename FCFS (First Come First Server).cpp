0a811600a9e3fd18be8542905656a35bbf1a3ebc
Fahim <dfahim432@gmail.com>
2023-02-20 08:35:17 +0600

    FCFS

---
+++ FCFS (First Come First Server).cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, x;
+    cin >> n;
+    vector<pair<int, int>> Burst;
+    vector<int> BT(n + 1);
+    map<int, int> TurnOver;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> x;
+        BT[i] = x;
+        Burst.push_back({x, i});
+    }
+    int turn = 0;
+
+    for (auto a : Burst)
+    {
+
+        turn += a.first;
+        TurnOver[a.second] = turn;
+    }
+    
+    cout << "TASK :  ";
+    for (int i = 1; i <= n; i++)
+    {
+        cout << i << " ";
+    }    
+    cout << endl<< "TURN :  ";
+    int turnAround=0,waitingTime=0;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << TurnOver[i] << " ";
+        turnAround+=TurnOver[i];
+    }
+    cout << endl<< "Wait :  ";
+    for (int i = 1; i <= n; i++)
+    {
+        cout << TurnOver[i] - BT[i] << " ";
+        waitingTime += TurnOver[i] - BT[i];
+    }
+
+    cout << endl;
+    cout << "AVG TURNOVER : " << double(turnAround)/n<<endl;
+    cout << "AVG WAITING : " << double(waitingTime) / n << endl;
+    cout
+        << "THE GANTTCHART: " << endl
+        << 0;
+    for (auto a : Burst)
+    {
+        cout << "---P" << a.second << "---" << TurnOver[a.second];
+    }
+}
